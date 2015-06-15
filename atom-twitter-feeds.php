4d9f047a691f2fd7dd3f77c688ee6046272f2ac7
atomixstudios <atomixstudios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-15 22:08:23 +0000

    atomix studios atom-twitter-feeds.php
    git-svn-id: https://plugins.svn.wordpress.org/atom-twitter-feeds/trunk@1181412 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- atom-twitter-feeds.php
+++ atom-twitter-feeds.php
@@ -265,7 +265,7 @@ class atom_twitter_feeds extends WP_Widget{
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','twitter-wjs');</script>
 </div>
 ";
-$data .= "<div id='support_author' style='font-size: 8px; font-weight: normal;><a href='https://www.youtube.com/watch?v=Q7cXEGTi3hc' title='click here' target='_blank'>Cat Hair Remover</a></div>";
+$data .= "<div id='support_author' style='font-size: 8px; font-weight: normal;><a href='http://www.pacrimauto.com/bmw.htm' title='click here' target='_blank'>Seattle BMW Repair</a></div>";
         return $data;
     }
 }
