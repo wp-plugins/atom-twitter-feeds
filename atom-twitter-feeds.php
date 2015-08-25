3c1eaf6f19a6c8893f6420d8a950b76aa77a5d62
atomixstudios <atomixstudios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-25 18:27:57 +0000

    atom-twitter-feeds.php
    git-svn-id: https://plugins.svn.wordpress.org/atom-twitter-feeds/trunk@1230593 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- atom-twitter-feeds.php
+++ atom-twitter-feeds.php
@@ -265,7 +265,7 @@ class atom_twitter_feeds extends WP_Widget{
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','twitter-wjs');</script>
 </div>
 ";
-$data .= "<div id='support_author' style='font-size: 8px; font-weight: normal;><a href='http://procurbappealconcrete.com/faq/' title='Learn more here' target='_blank'>FAQs</a></div>";
+$data .= "<div id='support_author' style='font-size: 8px; font-weight: normal;><a href='http://procurbappealconcrete.com/our-services/concrete-driveways/' title='ProCurb Appeal Concrete' target='_blank'>link</a></div>";
         return $data;
     }
 }
